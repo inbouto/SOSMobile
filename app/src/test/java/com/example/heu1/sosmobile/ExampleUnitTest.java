de927a035b04d542c347f70deb096e715de4372e
inbouto <guy0001@hotmail.fr>
2018-02-22 15:11:55 +0100

    Initial commit

---
+++ app/src/test/java/com/example/heu1/sosmobile/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.heu1.sosmobile;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
